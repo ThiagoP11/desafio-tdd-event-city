1a7ac77061c88f4e52f5f6b986049df4995b194c
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-10-23 15:54:28 -0300

    Test findAllShouldReturnAllResourcesSortedByName passing

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.bds02.services;
+
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.respositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(CityDTO::new).collect(Collectors.toList());
+    }
+
+
+
+}
